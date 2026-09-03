41bdf930247f979da37350982d2138df97fdf7e3
flesentine <chrisgmcc@gmail.com>
2026-09-03 16:14:27 -0700

    fix(v41): keep modified generic demonstratives anaphoric

--- src/generation_contract_v41_identity_choice_guard.js
+++ src/generation_contract_v41_identity_choice_guard.js
@@ -25,9 +25,13 @@ const ERA_BOUNDARY_START = `(?:(?:${ERA_DISCOURSE_MARKER})[,;:]?\\s+)*${ERA_INDE
 const ERA_DISCOURSE_NOUN_BOUNDARY_START = `(?:(?:${ERA_DISCOURSE_MARKER})[,;:]?\\s+)+${ERA_NOUN_CLAUSE_START}`;
 const DEMONSTRATIVE = "(?:this|that|these|those)";
 const DEMONSTRATIVE_PRONOUN_FOLLOW = new Set([
-  "a", "an", "any", "as", "actually", "also", "better", "bad", "cheap", "cool",
-  "even", "expensive", "fine", "fun", "good", "great", "okay", "ok", "really",
-  "still", "so", "terrible", "too", "very", "well", "worse", "worth",
+  "a", "an", "any", "as", "actually", "also", "awesome", "awful", "bad", "better",
+  "big", "black", "blue", "broken", "cheap", "cool", "even", "expensive", "fine",
+  "fun", "good", "great", "gray", "green", "grey", "huge", "large", "little",
+  "new", "newer", "nice", "old", "older", "okay", "ok", "original", "other",
+  "purple", "really", "red", "regular", "same", "silver", "small", "so", "standard",
+  "still", "terrible", "tiny", "too", "used", "very", "weird", "well", "white",
+  "working", "worse", "worth", "yellow",
   "is", "are", "was", "were", "did", "does", "do", "has", "have", "had",
   "can", "could", "would", "will", "should"
 ]);
