bacfa46131812cc41b21d230ed7c48581012b16f
flesentine <chrisgmcc@gmail.com>
2026-09-06 18:00:02 -0700

    Phase 3G.7 update compatibility authority gate

--- scripts/check_v41_coherence_repair_3c.mjs
+++ scripts/check_v41_coherence_repair_3c.mjs
@@ -85,10 +85,10 @@ function ownsMethod(source, name) {
 }
 
 assert.ok(wrapper.includes('from "./index_v41_human_reconnect.js"'));
-assert.ok(wrapper.includes('from "./index_v37_hotfix.js"'));
+assert.ok(wrapper.includes('from "./index_v37.js"'));
 assert.ok(!wrapper.includes('from "./index_v38_quality_guard.js"'));
-assert.ok(wrapper.includes("V37HotfixChatRoom.prototype.resolveDirectTarget.call"));
-assert.ok(wrapper.includes("V37HotfixChatRoom.prototype.voiceBrainPlan.call"));
+assert.ok(wrapper.includes("V37ChatRoom.prototype.resolveDirectTarget.call"));
+assert.ok(wrapper.includes("V37ChatRoom.prototype.voiceBrainPlan.call"));
 assert.ok(generationBase.includes('from "./index_v41_bot_roster_reentry.js"'));
 assert.ok(rosterWrapper.includes('from "./index_v41_world_date_guard.js"'));
 assert.ok(worldDateWrapper.includes('from "./index_v41_coherence_repair.js"'));
